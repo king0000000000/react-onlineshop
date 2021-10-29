ca91060e6524fba21c16f93e04db7072ff864ddf
king0000000000 <laes9210@hotmail.com>
2021-10-29 09:35:46 +0800

    initial commit

---
+++ backend/data/products.js
@@ -0,0 +1,39 @@
+const products = [
+    {
+        name: "PS5 光碟版遊戲主機",
+        imageUrl: "https://img.ruten.com.tw/s2/8/08/66/22110091752550_859.jpg",
+        description: "PlayStation5是SONY旗下的第5代遊戲機，具備強大的運算效能以及超高的讀取速度，以及具備了自適應扳機等新功能的專用控制器，將為所有玩家帶來更家沉浸的遊戲體驗。",
+        price: 15980,
+        countInStock: 3
+    },
+    {
+        name: "Iphone 12 Pro Max 128G",
+        imageUrl: "https://img.ruten.com.tw/s1/2/33/1d/22048076276509_176.jpg",
+        description: "iPhone 12 Pro Max是由蘋果公司設計和銷售的智慧型手機，它是繼iPhone 11 Pro Max後的第14代iPhone旗艦智慧型手機。iPhone 12 Pro Max在2020年10月13日於加利福尼亞州庫比蒂諾的蘋果園區舉行的發布活動上跟iPhone 12及iPhone 12 mini一同發布。由於嚴重特殊傳染性肺炎疫情關係，該發布會改為線上直播進行。iPhone 12 Pro Max的預訂日期始於2020年11月6日，並於2020年11月13日在大部分地區發售。",
+        price: 36000,
+        countInStock: 27
+    },
+    {
+        name: "nVIDIA GEFORCE RTX3080ti 顯示卡",
+        imageUrl: "https://cdn0.techbang.com/system/excerpt_images/87463/post_inpage/ed9c503922e9e2bd0277aa8946342182.jpg?1623161709",
+        description: "GeForce RTX™ 3080 Ti 和 RTX 3080 顯示卡採用 NVIDIA 的第二代 RTX Ampere 架構，可為玩家提供夢寐以求的極致效能。這兩款系統採用經過強化的 RT 核心和 Tensor 核心、全新串流多處理器，以及超高速 G6X 記憶體，打造出登峰造極的遊戲體驗。",
+        price: 38000,
+        countInStock: 15
+    },
+    {
+        name: "BANDIA MGEX 1:100 獨角獸鋼彈 Ver.Ka",
+        imageUrl: "https://img.ruten.com.tw/s2/c/ac/78/22105885439096_457.jpg",
+        description: "「MGEX 1/100 獨角獸鋼彈 Ver.Ka」本體採用新規造型製作，並由人氣機械設計師 Katoki Hajime 徹底監修，特色的「毀滅模式」變形機構經過重新設計，變形方式更加直觀、流暢。同時也附上新規設計的機身標誌水貼紙。\n而最大賣點則是這次挑戰的主題「極限的發光機構」。採用省空間、可彎曲的新材料「LED軟燈片（FLEXIBLE LED SHEET）」，在 1/100 比例的骨架、盾牌內置入薄型 LED 燈，連結底座的電源後即可讓透明的精神感應骨架發光，在發出紅光、綠光後還會調整強弱變化進行演出。讓變形、發光、色調變化、可動集結於一身。\n武裝部份則附上光束麥格農步槍、超絕火箭炮、感應盾牌*3、光束格林機槍*6、光束軍刀*4。大型的專用臺座可用來懸掛武裝、收納電池與線路，並演出格納庫中的整備狀態、覺醒後操縱三枚感應盾牌的場景。",
+        price: 5000,
+        countInStock: 31
+    },
+    {
+        name: "Union Creative UC 無職轉生 艾莉絲‧柏雷亞斯‧格瑞拉特",
+        imageUrl: "https://img.ruten.com.tw/s2/a/b0/b8/22106888796344_136.jpg",
+        description: "Union Creative出品，動漫\"無職轉生 到了異世界就拿出真本事\"中，人氣角色\"狂犬\"艾莉絲‧柏雷亞斯‧格瑞拉特的角色手辦模型。\n預計將在2021年8月發售。",
+        price: 3500,
+        countInStock: 10
+    }
+];
+
+module.exports = products;
